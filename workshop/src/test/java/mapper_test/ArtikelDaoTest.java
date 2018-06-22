f44765bb0ecaa52817b0a8954768bdb92ec22812
DODO\Olaf <duijn.olaf@gmail.com>
2018-06-22 14:39:19 +0200

    ArtikelDaoTest skelet toegevoegd

---
+++ workshop/src/test/java/mapper_test/ArtikelDaoTest.java
@@ -0,0 +1,35 @@
+package mapper_test;
+
+import junit.framework.TestCase;
+
+public class ArtikelDaoTest extends TestCase {
+
+	public void testCreateArtikel() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetArtikel() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAlleArtikelen() {
+		fail("Not yet implemented");
+	}
+
+	public void testUpdateArtikelStringBigDecimalIntInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testUpdateArtikelArtikel() {
+		fail("Not yet implemented");
+	}
+
+	public void testDeleteArtikelInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testDeleteArtikelArtikel() {
+		fail("Not yet implemented");
+	}
+
+}
