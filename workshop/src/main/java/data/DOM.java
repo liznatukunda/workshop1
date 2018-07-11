ae3f7c58eddb6f5c34d77e897db99ff376850647
liznatukunda <liz2010b@gmail.com>
2018-07-11 10:42:11 +0200

    new Dom

--- workshop/src/main/java/data/DOM.java
+++ workshop/src/main/java/data/DOM.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 public class DOM {
@@ -38,9 +39,22 @@ public class DOM {
 		return urlNodes.item(0).getTextContent();
 	}
 	
+	public String getUrl(String db){
+		Node dbNode = rootElement.getElementsByTagName(db.toLowerCase()).item(0);
+		Element dbElement = (Element) dbNode;
+		return dbElement.getElementsByTagName("url").item(0).getTextContent();
+	}
+	
+	
 	public String getDriverName(){
 		NodeList driverNodes = rootElement.getElementsByTagName("drivername");
 		return driverNodes.item(0).getTextContent();
+	}
+	
+	public String getDriverName(String db){
+		Node dbNode = rootElement.getElementsByTagName(db.toLowerCase()).item(0);
+		Element dbElement = (Element) dbNode;
+		return dbElement.getElementsByTagName("drivername").item(0).getTextContent();
 	}
 	
 	public String getUsername(){
@@ -48,8 +62,31 @@ public class DOM {
 		return userNodes.item(0).getTextContent();
 	}
 	
+	public String getUsername(String db){
+		Node dbNode = rootElement.getElementsByTagName(db.toLowerCase()).item(0);
+		Element dbElement = (Element) dbNode;
+		return dbElement.getElementsByTagName("username").item(0).getTextContent();
+	}
+	
 	public String getPassword(){
 		NodeList passNodes = rootElement.getElementsByTagName("password");
 		return passNodes.item(0).getTextContent();
 	}
+	
+	public String getPassword(String db){
+		Node dbNode = rootElement.getElementsByTagName(db.toLowerCase()).item(0);
+		Element dbElement = (Element) dbNode;
+		return dbElement.getElementsByTagName("password").item(0).getTextContent();
+	}
+    public String getPort(String db){
+        Node dbNode = rootElement.getElementsByTagName(db.toLowerCase()).item(0);
+        Element dbElement = (Element) dbNode;
+        return dbElement.getElementsByTagName("port").item(0).getTextContent();
+    }
+    
+    public String getDatabase(String db){
+        Node dbNode = rootElement.getElementsByTagName(db.toLowerCase()).item(0);
+        Element dbElement = (Element) dbNode;
+        return dbElement.getElementsByTagName("database").item(0).getTextContent();
+    }
 }
