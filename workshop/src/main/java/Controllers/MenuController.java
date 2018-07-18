9febbe507d654ddfbbbbf139eefecf11a35b7475
Pascal <hatricpassie@hotmail.com>
2018-07-18 10:17:10 +0200

    ter verhelping van verbinding met MongoDB

--- workshop/src/main/java/Controllers/MenuController.java
+++ workshop/src/main/java/Controllers/MenuController.java
@@ -4,21 +4,21 @@ import data.DaoFactory;
 import data.DatabaseSoort;
 
 public final class MenuController {
-	private static DatabaseSoort databasesoort;
+//	private static DatabaseSoort databasesoort;
 	
 	public static void setDatabase(int database) {
 		if (database==1) {
 			DaoFactory.setDatabaseMYSQL(true);
 		}
 		else {
-			databasesoort=DatabaseSoort.MONGODB;
+			DaoFactory.setDatabaseMYSQL(false);
 		}
 	}
 	
-	public static DatabaseSoort getDatabase() {
+/*	public static DatabaseSoort getDatabase() {
 		return databasesoort;
 	}
-
+*/
 	public static void setConnectionPool(int connectionPool) {
 		if(connectionPool == 1) {
 			data.ConnectieFactory.setConnectiePool(true);
